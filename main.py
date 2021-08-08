ca92a2e43821cb6dda5d77302743ba1cbc26b0a9
Dhruv Hegde <69629876+DDVHegde100@users.noreply.github.com>
2021-08-08 10:17:28 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,20 +3,20 @@ val=str(input('Enter the output cryptocurrency code:'))
 if(val=='BTC'):
     USD=float(input('Enter the amount in USD:'))
     BTC=USD*0.000022
-    print('%0.3f USD is equal to %0.3f bitcoin' %(USD, BTC))
+        print('%0.3f USD is equal to %0.3f bitcoin' %(USD, BTC))
 elif(val=='ETH'):
     USD=float(input('Enter the amount in USD:'))
     ETH=USD*0.000321
-    print('%0.3f USD is equal to %0.3f ethereum' %(USD, ETH))
+        print('%0.3f USD is equal to %0.3f ethereum' %(USD, ETH))
 elif(val=='LTC'):
     USD=float(input('Enter the amount in USD:'))
     LTC=USD*0.0064623264
-    print('%0.3f USD is equal to %0.3f litecoin' %(USD, LTC))
+        print('%0.3f USD is equal to %0.3f litecoin' %(USD, LTC))
 elif(val=='ADA'):
     USD=float(input('Enter the amount in USD:'))
     ADA=USD*0.681243
-    print('%0.3f USD is equal to %0.3f cardano' %(USD, ADA))
+        print('%0.3f USD is equal to %0.3f cardano' %(USD, ADA))
 elif(val=='DOGE'):
     USD=float(input('Enter the amount in USD:'))
     DOGE=USD*4.757766
-    print('%0.3f USD is equal to %0.3f dogecoin' %(USD, DOGE))
+        print('%0.3f USD is equal to %0.3f dogecoin' %(USD, DOGE))
